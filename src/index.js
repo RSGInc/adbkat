4116d7bdc71e73f7ac796889bbf7238bcff86ad4
Ariel Ryan-Pagano <ariel.ryan-pagano@rsginc.com>
2016-10-13 20:55:30 -0400

    Add adbkat.getDevices

--- src/index.js
+++ src/index.js
@@ -4,3 +4,22 @@
  * @module adbkat
  * @author RSG, Inc.
  */
+const adbkit = require(`adbkit`),
+    Device   = require(`./device`);
+
+module.exports = {
+    /**
+     * Retrieves the {@link Device}s that represent the devices that are
+     * connected to the computer via ADB.
+     *
+     * @return {Promise<Device[]>} A promise that resolves with the devices that
+     * are connected to the computer via ADB.
+     */
+    getDevices: () => {
+        const client = adbkit.createClient();
+
+        return client.listDevices().then(devices =>
+            Promise.all(devices.map(device => new Device(client, device)))
+        );
+    }
+};
